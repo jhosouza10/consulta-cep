8dff5ce27493acf98cfd0d99dabf68bc13b22856
Jhonatan Ferreira <=>
2022-09-29 23:21:29 -0300

    Commitando a primeira versão do projeto

---
+++ JavaScript/consulta-endereco.js
@@ -0,0 +1,32 @@
+
+/*declarando as variaveis*/
+
+let cep = document.querySelector('#cep');
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado = document.querySelector('#estado');
+
+/*declarando as formatação do CEP*/
+
+cep.value = '01001000';
+
+/*adicionando função e vinculando a API*/
+
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value
+    let script = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
+    document.body.appendChild(script);
+})
+
+/*puxando as informações conforme o campo da API*/
+
+function popularForm(resposta) {
+
+    rua.value = resposta.logradouro;
+    bairro.value = resposta.bairro;
+    cidade.value = resposta.localidade;
+    estado.value = resposta.uf;
+
+}
